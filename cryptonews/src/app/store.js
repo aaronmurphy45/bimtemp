5874e5f94deb639aed35d0a91ff7e87e8124e45b
Aaron Murphy <aaron@checkventory.com>
2021-11-29 13:10:31 +0000

    commit

---
+++ cryptonews/src/app/store.js
@@ -0,0 +1,18 @@
+import { configureStore } from '@reduxjs/toolkit'
+
+import { cryptoApi } from '../services/cryptoAPI';
+import { cryptoNewsApi } from '../services/cryptoNewsAPI';
+import { stockListApi } from '../services/stockListAPI';
+import { stockTimeSeriesApi } from '../services/stockTimeSeriesAPI';
+import { yahooRecommmend } from '../services/yahooRecommmend';
+
+
+export default configureStore({
+    reducer : {
+        [cryptoApi.reducerPath]: cryptoApi.reducer,
+        [cryptoNewsApi.reducerPath]: cryptoNewsApi.reducer,
+        [stockListApi.reducerPath]: stockListApi.reducer,
+        [stockTimeSeriesApi.reducerPath]: stockTimeSeriesApi.reducer,
+        [yahooRecommmend.reducerPath]: yahooRecommmend.reducer,
+    },
+});
